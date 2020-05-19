586f90c1f612c128f0ac31cd6fb8e9b7be77116c
masakazu <rururan0812@yahoo.co.jp>
2020-05-19 22:07:13 +0900

    fashionrecommend_app, first commit

--- fashion_app/settings.py
+++ fashion_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5m0-u4#6ug^#o0p(h=-d@-_q$(6=djgouq)!!^ln-hgwb6m(io'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
