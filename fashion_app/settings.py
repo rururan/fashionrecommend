d3608ddac23200abf109ee0a1cbf4d276c76ed67
masa <rururan0812@yahoo.co.jp>
2020-06-24 10:51:10 +0900

    0624_1051

--- fashion_app/settings.py
+++ fashion_app/settings.py
@@ -90,13 +90,6 @@ DATABASES = {
     }
 
 
-"""
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-#conn_max_age=600, ssl_require=True
-"""
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
