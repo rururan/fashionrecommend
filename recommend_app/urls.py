81fde84bfac8ae2b31b9e2c6dbe7fe0033be0d65
masa <rururan0812@yahoo.co.jp>
2020-06-24 16:08:01 +0900

    0624_1607

--- recommend_app/urls.py
+++ recommend_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name="recommend_app"
 urlpatterns = [
     path("",views.IndexView.as_view(),name="index"),
+    path("about",views.about,name="about"),
     path("input",views.input,name="input"),
     path("input_women",views.input_women,name="input_women"),
     path("result",views.result,name="result"),
